d879a9cf58ef2fa3193f751bd6807cb244c37684
Dmytro Iovenko <iovenko@gmail.com>
2024-09-17 16:33:10 -0400

    add Homepage and styling

---
+++ src/components/Homepage.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const HomePageContainer = styled.div`
+  font-family: Arial, sans-serif;
+  padding: 10px;
+`;
+
+export default function Homepage() {
+  return <HomePageContainer />;
+}
